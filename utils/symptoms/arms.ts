8b63d6cc2e539a1225b60b8dcba9e7e4c5c206d9
Eshan05 <nahareshan05@gmail.com>
2025-10-08 09:14:00 +0530

    feat: Start work on symptom selector 4

---
+++ utils/symptoms/arms.ts
@@ -0,0 +1,11 @@
+export const armSubCats = {
+  ALLARMS: 'All Arm Symptoms',
+  SHOULDER: 'Shoulder Symptoms',
+  ARMPIT: 'Armpit Symptoms',
+  UPPERARM: 'Upper Arm Symptoms',
+  ELBOW: 'Elbow Symptoms',
+  FOREARM: 'Forearm Symptoms',
+  WRIST: 'Wrist Symptoms',
+  HAND: 'Hand Symptoms',
+  FINGERS: 'Fingers Symptoms',
+}
