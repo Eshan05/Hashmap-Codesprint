23aac93dc9e705b820b51be607f6df1d41ec0920
Eshan05 <nahareshan05@gmail.com>
2025-10-09 17:40:00 +0530

    build: Use react compiler

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: process.env.ANALYZE === 'true', })
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactCompiler: true,
+  reactStrictMode: true,
+  compiler: { removeConsole: process.env.NODE_ENV === "production" },
+  // compiler: { removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false, },
+  images: {
+    minimumCacheTTL: 2678400,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
 };
 
-export default nextConfig;
+export default nextConfig
+// export default withBundleAnalyzer(nextConfig)
+// module.exports = withBundleAnalyzer(nextConfig)
